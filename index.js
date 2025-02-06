d8febc237cf0a62c20174018d038dbace3a9fcd4
Arman <collaboraone@gmail.com>
2025-02-06 14:05:11 +0530

    modified:   index.js

--- index.js
+++ index.js
@@ -14,14 +14,14 @@ cloudinary.config({
   secure: true,
 });
 
-app.use(cors({ 
-    origin: "https://event-management-arman.netlify.app", // Allow only your frontend
-    methods: "GET,POST,PUT,DELETE",
-    credentials: true
-}));
+
 
 const app = express();
-app.use(cors());
+app.use(cors({ 
+  origin: "https://event-management-arman.netlify.app", // Allow only your frontend
+  methods: "GET,POST,PUT,DELETE",
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
